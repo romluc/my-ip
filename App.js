640d76a96d79a7b2e510381a40644b8f51f94529
romluc <romulofilhofilho@gmail.com>
2019-09-02 18:59:06 -0300

    Add logo

--- App.js
+++ App.js
@@ -1,12 +1,18 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, Image } from 'react-native';
+
+import logo from './assets/logo.png';
 
 export default function App() {
 	return (
 		<View style={styles.container}>
 			<View style={styles.body}>
+				<Image source={logo} />
 				<Text style={styles.ip}>IP</Text>
-				<Button title='Descobrir meu IP!'></Button>
+				<Button title='Give me my IP!'></Button>
+			</View>
+			<View style={styles.footer}>
+				<Text style={styles.made}>Made by romluc 2019</Text>
 			</View>
 		</View>
 	);
@@ -30,5 +36,15 @@ const styles = StyleSheet.create({
 		color: '#fff',
 		paddingTop: 20,
 		paddingBottom: 20
+	},
+
+	footer: {
+		paddingTop: 10,
+		paddingBottom: 10
+	},
+
+	made: {
+		color: '#fff',
+		textAlign: 'center'
 	}
 });
